037ec8d34c9474188c7d0a04d4695ac92b828caa
Justin Barfield <barfieldj@jacks.sfasu.edu>
2025-04-15 17:50:17 -0500

    Add project files.

---
+++ FarmersMarketTest/ViewModels/FarmersMarketVM.cs
@@ -0,0 +1,47 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FarmersMarketTest.ViewModels
+{
+    class FarmersMarketVM : INotifyPropertyChanged
+    {
+        public ObservableCollection<StandVM> Items { get; } = new ObservableCollection<StandVM>();
+        public ICommand EnterCommand { get; }
+        public FarmersMarketVM()
+        {
+            EnterCommand = new RelayCommand(Enter);
+        }
+        private int numberOfStalls;
+        public int NumberOfStalls
+        {
+            get { return numberOfStalls; }
+            set
+            {
+                numberOfStalls = value;
+                OnPropertyChanged();
+            }
+        }
+        private void Enter()
+        {
+            Items.Clear(); // reset on each entry
+            for (int i = 1; i <= NumberOfStalls; i++)
+            {
+                Items.Add(new StandVM { FarmerID = $"Stall {i}" });
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
